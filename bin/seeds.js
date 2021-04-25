57a3955f87c842e0e29190398a9c7f8c0ad796dc
celia22 <celialopez22@gmail.com>
2021-04-25 18:39:19 +0200

    finish lab with styles

--- bin/seeds.js
+++ bin/seeds.js
@@ -14,7 +14,7 @@ mongoose.connect(`mongodb://localhost/${DB_NAME}`, {
 const drones = [
   { name: 'Creeper XL 500', propellers: 3, maxSpeed: 12 },
   { name: 'Racer 57', propellers: 4, maxSpeed: 20 },
-  { name: 'Courier 3000i', propellers: 6, maxSpeed: 18 },
+  { name: 'Courier 3000i', propellers: 6, maxSpeed: 18 }
 ];
 
 Drone.create(drones)
